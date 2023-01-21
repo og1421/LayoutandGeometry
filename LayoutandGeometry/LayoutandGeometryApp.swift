51d82b5dc0bfed5c6281413593298fa1c893ddb2
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-21 15:33:24 -0300

    Initial Commit

---
+++ LayoutandGeometry/LayoutandGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutandGeometryApp.swift
+//  LayoutandGeometry
+//
+//  Created by Orlando Moraes Martins on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutandGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
