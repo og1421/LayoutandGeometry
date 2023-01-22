59fe6e2603afb4596c1a5840f4dbf9b0a86f49ff
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-22 14:32:35 -0300

    GeometryReader Layout

--- LayoutandGeometry/ContentView.swift
+++ LayoutandGeometry/ContentView.swift
@@ -7,43 +7,24 @@
 
 import SwiftUI
 
-struct OutherView: View{
-    var body: some View {
-        VStack{
-            Text("Top")
-            InnerView()
-                .background(.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
-            
-            GeometryReader { geo in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                    }
-            }
-            .background(.orange)
-            
-            Text("Right")
-        }
-    }
-}
-
 struct ContentView: View {
+    let colors: [Color] = [.red, .blue, .green, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        OutherView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach (0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
