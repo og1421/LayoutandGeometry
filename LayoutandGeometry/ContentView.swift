4a59ef0adff693a4c91a698bd5d43fab5be5137d
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-22 14:38:10 -0300

    Layout with geometry reader

--- LayoutandGeometry/ContentView.swift
+++ LayoutandGeometry/ContentView.swift
@@ -11,17 +11,18 @@ struct ContentView: View {
     let colors: [Color] = [.red, .blue, .green, .orange, .pink, .purple, .yellow]
     
     var body: some View {
-        GeometryReader { fullView in
-            ScrollView {
-                ForEach (0..<50) { index in
-                    GeometryReader { geo in
-                        Text("Row #\(index)")
-                            .font(.title)
-                            .frame(maxWidth: .infinity)
-                            .background(colors[index % 7])
-                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack (spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader{ geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8 , axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
                     }
-                    .frame(height: 40)
+                    .frame(width: 200, height: 200)
                 }
             }
         }
