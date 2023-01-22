f5805494e7ed34066522cefd858035388fbba598
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-22 13:46:58 -0300

    Absolute positioning for SwiftUI Views

--- LayoutandGeometry/ContentView.swift
+++ LayoutandGeometry/ContentView.swift
@@ -7,34 +7,13 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAcountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAcountAndName.self)
-}
+
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName ){
-            VStack{
-                Text("@orlandogmmc")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center]}
-                Image("Apple_logo_grey.svg")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack{
-                Text("Full name")
-                Text("Orlando Moraes Martins")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center]}
-                    .font(.title)
-            }
-        }
+        Text("Hello World")
+            .position(x: 100, y:  100)
+            .background(.red)
     }
 }
 
